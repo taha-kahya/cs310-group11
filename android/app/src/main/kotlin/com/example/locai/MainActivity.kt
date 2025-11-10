3f6258cce5acdc29c10a3401e4c459ae6e2bd879
Ahmet Taha Kahya <taha.kahya@sabanciuniv.edu>
2025-11-10 15:24:11 +0300

    setup: get flutter environment ready

---
+++ android/app/src/main/kotlin/com/example/locai/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.locai
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+// TODO: Implement named routes
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'LocAI',
+      home: const MyHomePage(title: 'LocAI'),
+    );
+  }
+}
+
+// TODO: Implement the home page UI
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      )
+    );
+  }
+}
