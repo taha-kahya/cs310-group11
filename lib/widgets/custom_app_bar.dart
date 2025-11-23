ff4352a10a84595d3cf172769a385c2e24ae2947
Ahmet Taha Kahya <taha.kahya@sabanciuniv.edu>
2025-11-23 21:15:56 +0300

    fix(recent_searches): make cards consistent with home

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -4,7 +4,19 @@ import 'package:locai/utils/text_styles.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
-  const CustomAppBar({super.key, required this.title});
+  final bool showBack;
+  final bool showMenu;
+  final VoidCallback? onBack;
+  final VoidCallback? onMenu;
+
+  const CustomAppBar({
+    super.key,
+    required this.title,
+    this.showBack = false,
+    this.showMenu = true,
+    this.onBack,
+    this.onMenu,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -13,18 +25,30 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
       elevation: 0,
       centerTitle: true,
       automaticallyImplyLeading: false,
+      leading: showBack
+          ? IconButton(
+        icon: const Icon(Icons.arrow_back, color: Colors.black),
+        onPressed: onBack ?? () => Navigator.of(context).pop(),
+      )
+          : null,
       title: Text(
         title,
         style: AppTextStyles.heading,
       ),
-      actions: [
-        IconButton(
-          icon: const Icon(Icons.menu, color: Colors.black),
-          onPressed: () {
-            Scaffold.of(context).openEndDrawer();
-          },
+      actions: showMenu
+          ? [
+        Builder(
+          builder: (context) => IconButton(
+            icon: const Icon(Icons.menu, color: Colors.black),
+            onPressed: onMenu ??
+                    () {
+                  final scaffoldState = Scaffold.maybeOf(context);
+                  scaffoldState?.openEndDrawer();
+                },
+          ),
         ),
-      ],
+      ]
+          : null,
     );
   }
 
