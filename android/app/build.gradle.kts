21a66216d9cc37cce5aa7c4a5b28e9a95f76750b
can.demirtas <candemirtas004@gmail.com>
2026-01-05 23:24:50 +0300

    fix: remove unused api configs

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,7 +33,6 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-        manifestPlaceholders["MAPS_API_KEY"] = project.findProperty("MAPS_API_KEY") as String
     }
 
     buildTypes {
